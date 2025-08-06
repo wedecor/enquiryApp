457ed89182b430d915db4fa1a5b260a7c917aeed
IlyasWaseem <96107598+IlyasWaseem@users.noreply.github.com>
2025-08-06 19:07:55 +0530

    🔒 STEP 6: Successfully set up Vercel environment variables and deployed app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
