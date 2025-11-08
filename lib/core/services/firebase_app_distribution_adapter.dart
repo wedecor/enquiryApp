cad2025c7cc3ee5288d0b8abe1668512b2abdf46
IlyasWaseem <96107598+IlyasWaseem@users.noreply.github.com>
2025-11-09 04:35:32 +0530

    Revert "feat(update): trigger firebase app distribution updates"
    This reverts commit 71a9064deb595bba2a61ace99505a0b195abc7e3.

--- lib/core/services/firebase_app_distribution_adapter.dart
+++
@@ -1,7 +0,0 @@
-import 'firebase_app_distribution_adapter_interface.dart';
-import 'firebase_app_distribution_adapter_stub.dart'
-    if (dart.library.io) 'firebase_app_distribution_adapter_io.dart';
-
-FirebaseAppDistributionAdapter get firebaseAppDistributionAdapter =>
-    createFirebaseAppDistributionAdapter();
-
